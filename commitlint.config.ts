4fcac3bee6704dfda768037494570680bef6705e
codewithnuh <nuh25792@gmail.com>
2024-12-08 12:25:47 +0500

    chore: add Docker and Husky for automated testing and linting

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "chore", // Maintenance tasks
+        "docs", // Documentation changes
+        "style", // Code style changes (formatting, etc.)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Adding or updating tests
+        "build", // Changes to build process or dependencies
+        "ci", // Changes to CI configuration
+        "revert", // Reverting changes
+        "wip", // Work in progress
+      ],
+    ],
+  },
 };
